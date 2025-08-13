a526f07f8ff9c992b9dcef70303a57cdeb840b6e
RD42 <dashr9230@gmail.com>
2025-08-13 11:18:47 -0700

    [server] Revert: b99b3283778162f2776e2d67add44cd11019c337

--- server/pickup.cpp
+++ server/pickup.cpp
@@ -8,9 +8,7 @@ DWORD dwPickupTime = TIME_COUNT(72000,90);
 CPickup::CPickup(int iType, VECTOR * vecPos, int iAmount)
 {
 	m_iType = iType;
-	m_vecPos.X = vecPos->X;
-	m_vecPos.Y = vecPos->Y;
-	m_vecPos.Z = vecPos->Z;
+	m_vecPos = *vecPos;
 	m_iAmount = iAmount;
 	m_bIsSpawned = TRUE;
 }
