520c6fb8c374c8fdc5bb7d8883db9ad2aef8ebb9
RD42 <dashr9230@gmail.com>
2025-08-27 11:06:49 -0700

    [server] Implement/match `n_SetGravity`

--- server/scrcustom.cpp
+++ server/scrcustom.cpp
@@ -295,6 +295,16 @@ static cell AMX_NATIVE_CALL n_GetWeaponName(AMX *amx, cell *params)
 	return 0;
 }
 
+static cell AMX_NATIVE_CALL n_SetGravity(AMX *amx, cell *params)
+{
+	float fGravity = (params[1] / 100.0f) * 0.008f;
+	if(!(fGravity > 0.0f && fGravity < 0.01f))
+		fGravity = 0.008f;
+	pNetGame->m_GameSettings.fGravity = fGravity;
+	pNetGame->SendGameSettingsToAll();
+	return 1;
+}
+
 static cell AMX_NATIVE_CALL n_ResetNormalGravity(AMX *amx, cell *params)
 {
 	float fGravity = (100 / 100.0f) * 0.008f;
@@ -464,7 +474,7 @@ AMX_NATIVE_INFO custom_Natives[] =
 	{ "ShowPlayerMarkers", n_ShowPlayerMarkers },
 	{ "SetWorldTime", n_SetWorldTime },
 	{ "GetWeaponName", n_GetWeaponName },
-//	{ "SetGravity", n_SetGravity },
+	{ "SetGravity", n_SetGravity },
 	{ "ResetNormalGravity", n_ResetNormalGravity }
 	{ "SetGamespeed", n_SetGamespeed },
 	{ "ResetNormalGamespeed", n_ResetNormalGamespeed },
